5d8a4f925b8c4fdf5f4374582ae34f1ad2c7a3ff
SeryakovVlad <seryakov.vlad@gmail.com>
2023-08-17 20:23:18 +0200

    Hello World

---
+++ Power of Two.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while n > 1:
+            n /= 2
+        if n == 1:
+            return True
+        else:
+            return False
+
+            
+print(Solution().isPowerOfTwo(40))
